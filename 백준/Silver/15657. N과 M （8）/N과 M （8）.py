2c378d4a8e8443e924b524b29d083afc6dd55d79
Green <82005623+chorokKim@users.noreply.github.com>
2025-01-08 22:04:35 +0900

    [Silver III] Title: N과 M (8), Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+N, M = map(int, stdin.readline().split())
+arr = sorted(list(map(int, stdin.readline().split())))
+sel = []
+
+def backtracking(k):
+    if len(sel) == M:
+        print(*sel)
+        return
+    for i in range(k, N):
+        sel.append(arr[i])
+        backtracking(i)
+        sel.pop()
+
+backtracking(0)
