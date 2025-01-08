a4d056ea017cdd9149025060f189310a37e6c5dd
Green <82005623+chorokKim@users.noreply.github.com>
2025-01-08 22:51:02 +0900

    [Silver II] Title: N과 M (10), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,23 @@
+from sys import stdin
+
+N, M = map(int, stdin.readline().split())
+arr = list(map(int, stdin.readline().split()))
+arr.sort()
+sel = []
+isused = [False] * N
+
+def backtracking(k):
+    if len(sel) == M:
+        print(*sel)
+        return
+    tmp = 0
+    for i in range(k, N):
+        if not isused[i] and tmp != arr[i]:
+            sel.append(arr[i])
+            isused[i] = True
+            tmp = arr[i]
+            backtracking(i + 1)
+            sel.pop()
+            isused[i] = False
+
+backtracking(0)
